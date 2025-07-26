662d3aa3c5631ec1b053440c2a038f2405efa947
daniel eades <danieleades@hotmail.com>
2025-07-26 08:34:28 +0100

    fixup

--- examples/sphinx/conf.py
+++ examples/sphinx/conf.py
@@ -4,8 +4,6 @@ author = 'daniel eades'
 
 extensions = ["myst_parser"]
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', "README.md"]
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
